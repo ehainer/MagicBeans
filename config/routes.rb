2433b8a88aedf8f9c6b1be64253e42623110351e
Eric Hainer <eric.hainer@gmail.com>
2016-09-07 11:06:36 -0600

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
 
-	resources :magic_beans do
-		uploadable :beans
-		croppable :beans
-	end
+	namespace :magic_beans do
+		resources :beans do
+			uploadable :beans
+			croppable :beans
+		end
 
-	#namespace :magic_beans do
-	#	resources :beans, only: [:show, :create, :update]
-#
-#	#	scope "beans" do
-#	#	end
-	#end
+		#scope "beans" do
+		#end
+	end
 end
