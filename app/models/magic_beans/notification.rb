be5ed38619150fb87b32cb064e0f59ebe53983ad
Eric Hainer <eric.hainer@gmail.com>
2016-09-08 14:24:15 -0600

    Set response from email delivery

--- app/models/magic_beans/notification.rb
+++ app/models/magic_beans/notification.rb
@@ -176,7 +176,7 @@ module MagicBeans
 					request << @mailer_arguments.compact
 					mail = mailer.to_s.classify.constantize.send method, *@mailer_arguments.compact
 				end
-				puts mail.deliver_later(wait: 5.seconds).to_yaml
+				response << mail.deliver_later(wait: 5.seconds)
 			end
 
 			def attach(file, name = nil)
