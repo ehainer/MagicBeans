d6c97811f4a7b5df5af083cba0257ce87d70f6d1
Eric Hainer <eric.hainer@gmail.com>
2016-09-08 14:26:49 -0600

    Removed method column from notifications table, not necessary

--- db/migrate/20160614203408_create_magic_beans_notifications.rb
+++ db/migrate/20160614203408_create_magic_beans_notifications.rb
@@ -3,7 +3,6 @@ class CreateMagicBeansNotifications < ActiveRecord::Migration
 		create_table :magic_beans_notifications do |t|
 			t.string :notifyable_type
 			t.integer :notifyable_id
-			t.string :method
 			t.string :to_phone
 			t.string :to_email
 			t.json :request
