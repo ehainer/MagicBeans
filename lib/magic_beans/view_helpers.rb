dc52f48fc3ff47e870341dedb55346a6f591d4ce
Eric Hainer <eric.hainer@gmail.com>
2016-09-19 14:29:25 -0600

    Swapped order of merging hashes

--- lib/magic_beans/view_helpers.rb
+++ lib/magic_beans/view_helpers.rb
@@ -2,7 +2,7 @@ module MagicBeans
 	module ViewHelpers
 
 		def svg_tag(source, image = nil, **options)
-			svg = MagicBeans::SVG.new(source, image, options.merge(asset_lookup: true))
+			svg = MagicBeans::SVG.new(source, image, { asset_lookup: true }.merge(options))
 			svg.html_tag
 		end
 	end
